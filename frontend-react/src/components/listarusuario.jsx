54c2a0ff0015d56adc68b82f308c46a0c2647481
Luis1080931 <luisu0616@gmail.com>
2024-02-08 11:38:37 -0500

    cambios juan

--- frontend-react/src/components/listarusuario.jsx
+++ frontend-react/src/components/listarusuario.jsx
@@ -1,28 +1,23 @@
 import React, { useEffect, useState, useRef } from "react";
 import Api from "../services/api";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
-import '../style/usuarios.css';
 import Sweet from "../helpers/Sweet";
-
-
+import '../style/usuarios.css';
 
 const ListarUsuarios = () => {
     const [usuarios, setUsuarios] = useState([]);
-    const [idUsuario, setIdUsuario] = useState();
     const [isRegistrarModalOpen, setRegistrarModalOpen] = useState(false);
+    const [isActualizarModalOpen, setActualizarModalOpen] = useState(false);
+    const [usuarioToUpdate, setUsuarioToUpdate] = useState(null);
     const [fincas, setFincas] = useState([]);
     const [municipios, setMunicipios] = useState([]);
     const [selectedMunicipio, setSelectedMunicipio] = useState("");
-
-
-    const usuarios_id = useRef();
     const nombre = useRef();
-    const longitud = useRef();
-    const latitud = useRef();
-    const municipios_id = useRef();
-    const noombre_vereda = useRef();
-
+    const apellido = useRef();
+    const numeroDocumento = useRef();
+    const telefono = useRef();
+    const correo = useRef();
+    const estado = useRef();
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -48,30 +43,7 @@ const ListarUsuarios = () => {
             }
         };
         buscarUsuarios();
-
     }, []);
-    useEffect(() => {
-        let inputRegister = document.querySelectorAll(".input-register");
-        let h6Error = document.querySelectorAll(".h6-error");
-
-        for (let x = 0; x < inputRegister.length; x++) {
-            inputRegister[x].addEventListener("change", function () {
-                let h6Error = document.querySelectorAll(".h6-error");
-
-                if (h6Error[x]) {
-                    h6Error[x].style.display = "none"
-                }
-            })
-            inputRegister[x].addEventListener("input", function () {
-                let h6Error = document.querySelectorAll(".h6-error");
-
-                if (h6Error[x]) {
-                    h6Error[x].style.display = "none"
-                }
-            })
-        }
-
-    }, [isRegistrarModalOpen])
 
     const openRegistrarModal = () => {
         setRegistrarModalOpen(true);
@@ -81,77 +53,95 @@ const ListarUsuarios = () => {
         setRegistrarModalOpen(false);
     };
 
-    const handleRegistrar = async (data) => {
-        // Obtener el ID del usuario actualmente autenticado o ajustar según tu lógica
+    const openActualizarModal = (usuario) => {
+        setUsuarioToUpdate(usuario);
+        setActualizarModalOpen(true);
+    };
 
+    const closeActualizarModal = () => {
+        setActualizarModalOpen(false);
+    };
 
-        const fincaData = {
-            ...data
-        };
-
-        const headers = {
-            headers: {
-                token: "xd",
-            },
+    const handleRegistrar = async (e) => {
+        e.preventDefault();
+        const usuarioData = {
+            nombre: nombre.current.value,
+            apellido: apellido.current.value,
+            numero_documento: numeroDocumento.current.value,
+            telefono: telefono.current.value,
+            correo_electronico: correo.current.value,
+            estado: estado.current.value
         };
 
         try {
-            const data = await Api.post("finca/registrar", fincaData, headers);
-            if (data.data.status == false) {
-                let keys = Object.keys(data.data.errors)
-                let h6Error = document.querySelectorAll(".h6-error");
-                for (let x = 0; x < h6Error.length; x++) {
-                    h6Error[x].remove()
-                }
-                console.log(data.data)
-                for (let x = 0; x < keys.length; x++) {
-                    let h6 = document.createElement("h6")
-                    h6.innerHTML = data.data.errors[keys[x]]
-                    h6.classList.add("h6-error")
-                    if (document.getElementById(keys[x])) {
-                        let parent = document.getElementById(keys[x]).parentNode
-                        parent.appendChild(h6)
-                    }
-
-                }
-            } else {
-                console.log(data.data)
+            const response = await Api.post("usuario/registrar", usuarioData);
+            console.log(response.data);
+            if (response.data.status) {
                 Sweet.registroExitoso();
                 closeRegistrarModal();
-                // Recargar la lista de fincas después del registro
-                const response = await Api.get("finca/listar");
-                setFincas(response.data);
-                location.href = "/finca"
+                setUsuarios([...usuarios, response.data.usuario]);
+            } else {
+                console.error(response.data.message);
             }
-
-
-
         } catch (error) {
-            console.error("Error al registrar la finca:", error);
+            console.error("Error al registrar usuario:", error);
         }
     };
 
+    const handleActualizar = async (e) => {
+        e.preventDefault();
+        const usuarioData = {
+            id: usuarioToUpdate.id,
+            nombre: nombre.current.value,
+            apellido: apellido.current.value,
+            numero_documento: numeroDocumento.current.value,
+            telefono: telefono.current.value,
+            correo_electronico: correo.current.value,
+            estado: estado.current.value
+        };
+
+        try {
+            const response = await Api.post("usuario/actualizar", usuarioData);
+            console.log(response.data);
+            if (response.data.status) {
+                Sweet.registroExitoso();
+                closeActualizarModal();
+                const updatedUsuarios = usuarios.map((user) =>
+                    user.id === usuarioToUpdate.id ? response.data.usuario : user
+                );
+                setUsuarios(updatedUsuarios);
+            } else {
+                console.error(response.data.message);
+            }
+        } catch (error) {
+            console.error("Error al actualizar usuario:", error);
+        }
+    };
 
     return (
         <>
             {isRegistrarModalOpen && (
                 <div className="overlay" onClick={closeRegistrarModal}></div>
             )}
+            {isActualizarModalOpen && (
+                <div className="overlay" onClick={closeActualizarModal}></div>
+            )}
             <img src="../../public/img/fondo.png" alt="" className="fondo2" />
             <div className="tablalistar">
                 <h1 className="titu">Usuarios</h1>
+                <button className="btn-primary" onClick={openRegistrarModal}>Registrar Usuario</button>
                 <br />
                 <table className="tableprincipal">
                     <thead>
                         <tr>
                             <th>id</th>
-                            <th>nombre</th>
+                            <th>Nombre</th>
                             <th>Apellido</th>
-                            <th>Numero de documento</th>
-                            <th>Telefono</th>
-                            <th>correo</th>
+                            <th>Número de Documento</th>
+                            <th>Teléfono</th>
+                            <th>Correo Electrónico</th>
                             <th>Estado</th>
-                            <th>opciones</th>
+                            <th>Opciones</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -160,20 +150,12 @@ const ListarUsuarios = () => {
                                 <td>{usuario.id}</td>
                                 <td>{usuario.nombre}</td>
                                 <td>{usuario.apellido}</td>
-                                <td>{usuario.numero_documentos}</td>
+                                <td>{usuario.numero_documento}</td>
                                 <td>{usuario.telefono}</td>
                                 <td>{usuario.correo_electronico}</td>
-                                <td>{usuario.estado === 1 ? 'Activo' : 'Desactivado'}</td>
+                                <td>{usuario.estado === 1 ? 'Activo' : 'Inactivo'}</td>
                                 <td>
-                                    <Link to={`/usuario/actualizar/${usuario.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-                                        <button type="button" className="btn-primary">
-                                            actualizar
-                                        </button>
-                                    </Link>
-                                    <button className="btn-registrar" onClick={() => { setIdUsuario(usuario.id); openRegistrarModal() }}>
-                                        Registrar Finca
-                                    </button>
-
+                                    <button className="btn-icon-edit" onClick={() => openActualizarModal(usuario)}><i className="fas fa-pen"></i></button>
                                 </td>
                             </tr>
                         ))}
@@ -181,86 +163,60 @@ const ListarUsuarios = () => {
                 </table>
             </div>
             {isRegistrarModalOpen && (
-                <div className="overlay" onClick={closeRegistrarModal}></div>
-            )}
-
-            {isRegistrarModalOpen && (
-                <div className="tabla2">
-                    <h1 className="text-center font-bold underline text-3xl p-3 m-2">
-                        Registrar Finca
-                    </h1>
-
-                    <form
-                        className="contenido-regi"
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            handleRegistrar({
-
-                                nombre: nombre.current.value,
-                                longitud: longitud.current.value,
-                                latitud: latitud.current.value,
-                                usuarios_id: usuarios_id.current.value,
-                                municipios_id: selectedMunicipio,  // Utilizar el municipio seleccionado
-                                noombre_vereda: noombre_vereda.current.value
-                            });
-                        }}
-                        method="post"
-                    >
-
-
-
+                <div className="modal-registrar">
+                    <form onSubmit={handleRegistrar}>
                         <div className="div-input">
-                            <input className="input-register" type="text" id="nombre" name="nombre" ref={nombre} placeholder="" />
-                            <label htmlFor="nombre">Nombre</label>
+                            <input type="text" ref={nombre} placeholder="Nombre" required />
                         </div>
                         <div className="div-input">
-                            <input className="input-register" type="text" id="longitud" name="longitud" ref={longitud} placeholder="" />
-                            <label htmlFor="longitud">Longitud</label>
+                            <input type="text" ref={apellido} placeholder="Apellido" required />
                         </div>
                         <div className="div-input">
-                            <input className="input-register" type="text" id="latitud" name="latitud" ref={latitud} placeholder="" />
-                            <label htmlFor="latitud">Latitud</label>
+                            <input type="text" ref={numeroDocumento} placeholder="Número de Documento" required />
                         </div>
-
-                        <input value={idUsuario} type="hidden" id="usuarios_id" name="usuarios_id" ref={usuarios_id} placeholder="" />
-
                         <div className="div-input">
-
-                            <select
-                                className="input-register"
-                                id="municipios_id"
-                                name="municipios_id"
-                                value={selectedMunicipio}
-                                onChange={(e) => {
-                                    console.log("Municipio seleccionado:", e.target.value);
-                                    setSelectedMunicipio(e.target.value);
-                                }}
-                            >
-                                <option value="" disabled>Seleccione un municipio</option>
-                                {municipios.map((municipio) => (
-                                    <option key={municipio.id} value={municipio.id}>
-                                        {municipio.nombre}
-                                    </option>
-                                ))}
+                            <input type="text" ref={telefono} placeholder="Teléfono" required />
+                        </div>
+                        <div className="div-input">
+                            <input type="email" ref={correo} placeholder="Correo Electrónico" required />
+                        </div>
+                        <div className="div-input">
+                            <select ref={estado}>
+                                <option value="1">Activo</option>
+                                <option value="0">Inactivo</option>
                             </select>
                         </div>
-
+                        <button type="submit">Registrar Usuario</button>
+                        <button className="close-modal-btn" onClick={closeRegistrarModal}>Cerrar</button>
+                    </form>
+                </div>
+            )}
+            {isActualizarModalOpen && usuarioToUpdate && (
+                <div className="modal-actualizar">
+                    <form onSubmit={handleActualizar}>
                         <div className="div-input">
-                            <input className="input-register" type="text" id="noombre_vereda" name="noombre_vereda" ref={noombre_vereda} placeholder="" />
-                            <label htmlFor="noombre_vereda">nombre vereda</label>
+                            <input type="text" ref={nombre} defaultValue={usuarioToUpdate.nombre} required />
                         </div>
-                        <button
-                            className="btn-blue"
-                            type="submit"
-                        >
-                            Registrar finca
-                        </button>
-                        <button
-                            className="close-modal-btn"
-                            onClick={closeRegistrarModal}
-                        >
-                            Cerrar
-                        </button>
+                        <div className="div-input">
+                            <input type="text" ref={apellido} defaultValue={usuarioToUpdate.apellido} required />
+                        </div>
+                        <div className="div-input">
+                            <input type="text" ref={numeroDocumento} defaultValue={usuarioToUpdate.numero_documento} required />
+                        </div>
+                        <div className="div-input">
+                            <input type="text" ref={telefono} defaultValue={usuarioToUpdate.telefono} required />
+                        </div>
+                        <div className="div-input">
+                            <input type="email" ref={correo} defaultValue={usuarioToUpdate.correo_electronico} required />
+                        </div>
+                        <div className="div-input">
+                            <select ref={estado} defaultValue={usuarioToUpdate.estado}>
+                                <option value="1">Activo</option>
+                                <option value="0">Inactivo</option>
+                            </select>
+                        </div>
+                        <button type="submit">Actualizar Usuario</button>
+                        <button className="close-modal-btn" onClick={closeActualizarModal}>Cerrar</button>
                     </form>
                 </div>
             )}
