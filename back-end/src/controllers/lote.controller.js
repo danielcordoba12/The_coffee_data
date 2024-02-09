fd9e78a5fb06186424c744b808a77d84edc047ce
4Nunez4 <jorgeenrrique123456789@gmail.com>
2024-02-09 08:15:45 -0500

    modi

--- back-end/src/controllers/lote.controller.js
+++ back-end/src/controllers/lote.controller.js
@@ -16,7 +16,7 @@ export const listarlote= async (req,res)=>{
 };
 export const listarPorFinca= async (req,res)=>{
     try{
-        const[result]= await pool.query(`select l.id, l.fecha_creacion, l.nombre, l.latitud, l.longitud, f.nombre as nombre_finca, l.estado from lotes l join fincas f on f.id = l.fincas_id WHERE f.id = ${req.params.id} order by l.estado desc, l.fecha_creacion DESC`);
+        const[result]= await pool.query(`select l.id, l.fecha_creacion, l.nombre, l.latitud, l.longitud, f.nombre as nombre_finca,l.n_plantas, l.estado from lotes l join fincas f on f.id = l.fincas_id WHERE f.id = ${req.params.id} order by l.estado desc, l.fecha_creacion DESC`);
         res.status(200).json(result);
 
         console.log(result)
